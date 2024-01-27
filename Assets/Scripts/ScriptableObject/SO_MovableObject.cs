0d3b1ec2d8f91f415b71e2c5b8e65b703c5bd622
WIZI <loick.konieczny@gmail.com>
2024-01-27 08:38:09 +0100

    Revert "V1 of the Snap System"
    This reverts commit c4f678e66c88085453a929c76e60bbeef2ec5c76.

--- Assets/Scripts/ScriptableObject/SO_MovableObject.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "MovableObject", menuName = "ScriptableObjects/MovableObject", order = 1)]
-public class SO_MovableObject : ScriptableObject
-{
-    public bool isMovable;
-}
