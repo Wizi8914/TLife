9558363e9ffcaabb14d0319bc0c7cb9687ac2af3
Oxyderkeya <bierna.sandrine@gmail.com>
2024-01-27 18:45:55 +0100

    Move Prefabs + Add Prefabs Lvl2

--- Assets/Scripts/ClickableObject.cs
+++ Assets/Scripts/ClickableObject.cs
@@ -3,17 +3,28 @@ using UnityEngine;
 [RequireComponent(typeof(PolygonCollider2D))]
 public class ClickableObject : MonoBehaviour
 {
+    private Sprite startSprite;
     public Sprite newSprite;
 
     [SerializeField]
     private bool clicked = false;
 
+    private void Start()
+    {
+        startSprite = gameObject.GetComponent<SpriteRenderer>().sprite;
+    }
     private void OnMouseDown()
     {
-        if (clicked) return;
+        if (clicked)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = startSprite;
+            clicked = false;
+        } 
+        else
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = newSprite;
+            clicked = true;
+        }
 
-        gameObject.GetComponent<SpriteRenderer>().sprite = newSprite;
-        
-        clicked = true;
     }
 }
